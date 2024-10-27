9c1c306f78a42a38b89fbfc99863172e247f7862
poteto <naonyan1135@gmail.com>
2024-10-28 02:05:36 +0900

    buildしてなかった

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,2 @@
-(()=>{var c=document.querySelectorAll("[data-confirm]");c.forEach(e=>{e.addEventListener("click",t=>{let n=e.getAttribute("data-confirm");window.confirm(n)||t.preventDefault()})});})();
+(()=>{var o=document.querySelectorAll("[data-confirm]");o.forEach(e=>{let r=e.getAttribute("data-confirm"),t=e.dataset.trigger||"click",n=a=>{window.confirm(r)||a.preventDefault()};t==="hover"?e.addEventListener("mouseover",n):e.addEventListener(t,n)});})();
 //# sourceMappingURL=bundle.js.map
